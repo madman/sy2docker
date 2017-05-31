55ac70cbadd1b609dae94171a089423a564c15e0
Yuriy Prokopets <yy@bbq.agency>
2017-05-31 18:48:16 +0300

    App. Reworked prod/dev environment initialization

--- web/app.php
+++ web/app.php
@@ -1,12 +1,20 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
+$env  = $_SERVER['APP_ENV'] ?: 'prod';
+$debug  = (boolean)$_SERVER['DEBUG'];
+
+if ($debug) {
+	Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
